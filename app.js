dc61c4061b3a1db2a92c130bb54002532a96a0c0
Bappa Marjit <bappa.marjit98@gmail.com>
2024-07-25 13:24:23 +0530

    v1 portfolio

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+app.listen(3000,(req,res)=>{
+    console.log('listening on port 3000');
+});
+
+app.get("/", (req,res)=>{
+    res.render('home.ejs');
+});
+app.get('/contact', (req,res)=>{
+    res.render('contact.ejs');
+});
+app.get('/about',(req,res)=>{
+    res.render("about.ejs");
+})
